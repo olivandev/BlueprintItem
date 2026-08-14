f5343e3835287c87ad32e17dfe6ad93747313d8a
Oliver Van Dyk <53413246+olivandev@users.noreply.github.com>
2026-08-14 17:31:40 +1000

    Update plugin version to 1.0.1

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -3,7 +3,7 @@ namespace BlueprintItem
     public static class PluginInfo
     {
         public const string PLUGIN_NAME = "BlueprintItem";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.0.1";
         public const string PLUGIN_AUTHOR = "olivandev";
         public const string PLUGIN_GUID = $"{PLUGIN_AUTHOR}.Romestead.{PLUGIN_NAME}";
         public const string PLUGIN_AUTHOR_URL = "https://steamcommunity.com/id/souliiii/";
